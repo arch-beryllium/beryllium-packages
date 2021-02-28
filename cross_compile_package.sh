e6fe0876b76ca998b4d7c366a6fdd791dda3cf9a
jld3103 <jld3103yt@gmail.com>
2021-02-28 09:21:58 +0100

    Pass arguments down to makepkg

--- cross_compile_package.sh
+++ cross_compile_package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-LANG=C MAKEFLAGS=-j$(nproc --all) "$(dirname "$0")/makepkg.sh" -s -f -A --noconfirm --config "$(dirname "$0")/makepkg.conf"
+LANG=C MAKEFLAGS=-j$(nproc --all) "$(dirname "$0")/makepkg.sh" -s -f -A --noconfirm --config "$(dirname "$0")/makepkg.conf" "$@"
--- host_compile_package.sh
+++ host_compile_package.sh
@@ -4,7 +4,7 @@ set -ex
 root="$(dirname "$0")/rootfs"
 
 # To speed up dependency installation and source downloading and checking, do it on the host
-LANG=C MAKEFLAGS=-j$(nproc --all) "$(dirname "$0")/makepkg.sh" -s -f -A --noconfirm --verifysource --config "$(dirname "$0")/makepkg.conf"
+LANG=C MAKEFLAGS=-j$(nproc --all) "$(dirname "$0")/makepkg.sh" -s -f -A --noconfirm --verifysource --config "$(dirname "$0")/makepkg.conf" "$@"
 
 # Simply mount the package source directory to the rootfs
 cleanup() {
@@ -15,4 +15,4 @@ mkdir -p "$root"/mnt/pkg
 mount -o bind . "$root"/mnt/pkg
 
 # Now we can skip downloading and checking the sources and installing dependencies
-arch-chroot "$root" /bin/bash -c "set -ex && cd /mnt/pkg && LANG=C MAKEFLAGS=-j$(nproc --all) makepkg -f -A --noconfirm --skipinteg --holdver"
+arch-chroot "$root" /bin/bash -c "set -ex && cd /mnt/pkg && LANG=C MAKEFLAGS=-j$(nproc --all) makepkg -f -A --noconfirm --skipinteg --holdver \"$@\""
